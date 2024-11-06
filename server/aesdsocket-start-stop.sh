51687ae504a57abe3b02df962e27f4ce5f236423
Pranjal Gupta <prgu8117@aesd-students.colorado.edu>
2024-11-06 04:11:39 +0000

    testing for full test for assign 5

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,49 +1,20 @@
 #!/bin/sh
 
-# Path to the aesdsocket executable on the target system
-:<<END AESDSOCKET_PATH="/usr/bin/aesdsocket"
-PID_FILE="/var/run/aesdsocket.pid"
-
-start() {
-    echo "Starting aesdsocket..."
-    start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PID_FILE --exec $AESDSOCKET_PATH -- -d
-    if [ $? -eq 0 ]; then
-        echo "aesdsocket started."
-    else
-        echo "Failed to start aesdsocket."
-        exit 1
-    fi
-}
-
-stop() {
-    echo "Stopping aesdsocket..."
-    start-stop-daemon --stop --quiet --pidfile $PID_FILE --signal SIGTERM
-    if [ $? -eq 0 ]; then
-        echo "aesdsocket stopped."
-    else
-        echo "Failed to stop aesdsocket."
-        exit 1
-    fi
-    # Clean up PID file
-    if [ -f $PID_FILE ]; then
-        rm -f $PID_FILE
-    fi
-}
-END
 case "$1" in
     start)
+	echo "Starting aesdsocket"
         start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
+	echo "stopping aesdsocket"
         start-stop-daemon -K -n aesdsocket
         ;;
     restart)
-        stop
-        start
-        ;;
+	$0 stop
+    	$0 start	
     *)
         echo "Usage: $0 {start|stop|restart}"
-        exit 1
+    	exit 1
         ;;
 esac
 
