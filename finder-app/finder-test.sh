165db52841a2cb8c40dfe50135b35ee70af0cf34
dlee_debian <darrenlee821@gmail.com>
2024-11-10 02:13:32 +0800

    update finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -6,24 +6,28 @@
 set -e
 set -u
 
+# Ensure the PATH includes /usr/bin
+export PATH=$PATH:/usr/bin
+
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat /etc/finder-app/conf/username.txt)
+username=$(cat /home/dlee/a3/conf/username.txt)
+assignment=$(cat /home/dlee/a3/conf/assignment.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]
+    then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi  
 else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -33,21 +37,19 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # Change the configuration file path
-assignment=$(cat /etc/finder-app/conf/assignment.txt)
-
 if [ "$assignment" != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
-	if [ ! -d "$WRITEDIR" ]
-	then
-		echo "Failed to create directory $WRITEDIR"
-		exit 1
-	fi
+    mkdir -p "$WRITEDIR"
+    if [ ! -d "$WRITEDIR" ]
+    then
+        echo "Failed to create directory $WRITEDIR"
+        exit 1
+    fi
 fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
@@ -61,9 +63,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+    echo "success"
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
 fi
